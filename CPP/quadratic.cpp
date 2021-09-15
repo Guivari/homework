43fec85d2e217945cb4d5ed2605ad37f7e1493ad
Guivari <guivari.amri@gmail.com>
2021-09-15 12:08:19 +0200

    finalize quadratic.cpp

--- CPP/quadratic.cpp
+++ CPP/quadratic.cpp
@@ -2,9 +2,7 @@
 #include <cmath>
 
 int main() {
-    int a, b, c, discriminant;
-    double sol1, sol2;
-    double y, z;
+    float a, b, c, discriminant, sol1, sol2;
 
     std::cout << "Please enter the values of a, b, and c: ";
     std::cin >> a >> b >> c;
@@ -14,7 +12,7 @@ int main() {
         std::cout << "There is no solution.";
 
     } else if (discriminant == 0) {
-        sol1 = (-1 * b) / 2 * a;
+        sol1 = (-1.0*b) / (2.0*a);
         std::cout << "There is 1 solution.\n\nThe solution is: " << sol1;
 
     } else if (discriminant > 0) {
