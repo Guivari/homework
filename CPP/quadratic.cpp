04f14b0083bc63ffd483a32d54c3c0e2d28d11e5
Guivari <guivari.amri@gmail.com>
2021-09-15 11:45:44 +0200

    update quadratic cpp

--- CPP/quadratic.cpp
+++ CPP/quadratic.cpp
@@ -4,22 +4,26 @@
 int main() {
     int a, b, c, discriminant;
     double sol1, sol2;
+    double y, z;
+
     std::cout << "Please enter the values of a, b, and c: ";
     std::cin >> a >> b >> c;
-    
     discriminant = (b*b) - (4 * a * c);
-    std::cout << discriminant << std::endl;
+
     if (discriminant < 0) {
         std::cout << "There is no solution.";
+
     } else if (discriminant == 0) {
         sol1 = (-1 * b) / 2 * a;
         std::cout << "There is 1 solution. \nThe solution is: " << sol1;
+
     } else if (discriminant > 0) {
-        sol1 = ((-1.0*b) + sqrt(discriminant)) / 2*a;
-        sol2 = ((-1.0*b) - sqrt(discriminant)) / 2*a;
+        sol1 = ((-1.0*b) + sqrt(discriminant)) / (2.0*a);
+        sol2 = ((-1.0*b) - sqrt(discriminant)) / (2.0*a);
         std::cout << "There are 2 solutions. \nThe solutions are: " << sol1 << " and " << sol2;
     }
 
+    std::cout << std::endl;
 
     return 0;
 }
